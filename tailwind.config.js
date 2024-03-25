1589afd85760cc116c46e211eb46383ee396cfc8
talitha <talithasyafiyah1112@gmail.com>
2024-03-25 16:53:10 +0700

    Setting: color and container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF4242",
+        background: "#131313",
+        textcolor: "#2D2D2D",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          width: "100%",
+          paddingLeft: "16px",
+          paddingRight: "16px",
+          "@screen sm": {
+            maxWidth: "640px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            paddingLeft: "0",
+            paddingRight: "0",
+          },
+          "@screen md": {
+            maxWidth: "700px",
+          },
+          "@screen lg": {
+            maxWidth: "920px",
+          },
+          "@screen xl": {
+            maxWidth: "1120px",
+          },
+          "@screen 2xl": {
+            maxWidth: "1440px",
+          },
+        },
+      });
+    }),
+  ],
 };
