fbf62a5609639b5ac4504c3eb188f8513cd99e45
RafiDhafinErsamazaya <160104105+RafiDhafinErsamazaya@users.noreply.github.com>
2024-03-26 20:00:04 +0700

    Update peoplelist

--- src/Rafi/PeopleList.jsx
+++ src/Rafi/PeopleList.jsx
@@ -1,36 +1,106 @@
 import react, { useEffect, useState } from "react";
+import axios from "axios";
 export default function PeopleList() {
   const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  async function fetchdatawithasyncawait() {
+  const peopleList = async () => {
     try {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/person/changes?api_key=0389f7dff3b6002ad073a6a720cee29b"
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/person/popular?api_key=0389f7dff3b6002ad073a6a720cee29b&page=${currentPage}&language=en-US`,
+        { headers: { accept: "application/json" } }
       );
-      if (!response.ok) {
-        throw new error("failed to fetch data");
-      }
-      const items = await response.json();
-      console.log("data received with async/await", data);
-      setData(items);
+      console.log("People data ", response.data);
+      setData(response.data.results);
+      setTotalPages(response.data.total_pages);
     } catch (error) {
-      console.error("Error", error);
+      console.error("Error fetching data: ", error);
     }
-  }
+  };
+
   useEffect(() => {
-    fetchdatawithasyncawait();
-  }, []);
+    peopleList();
+  }, [currentPage]);
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const goToPrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
 
   return (
-    <div className="p-4">
-      <div className="bg-gray-800 text-white p-4 rounded-lg">
-        <h1 className="text-2xl font-bold mb-4">People List</h1>
-        {peopleList.map((e) => (
-          <div key={e.id} className="bg-gray-700 rounded-lg p-4 mb-4">
-            <div className="text-lg font-bold mb-1">{e?.name}</div>
-            <div className="text-sm mb-1">{e?.known_for_department}</div>
-            <div className="text-sm mb-1">{e?.original_name}</div>
-            <div className="text-sm">{e?.popularity}</div>
+    <div className="container px-4">
+      <div>
+        <h1 className="text-4xl text text-center text-red-500 font-semibold py-16">
+          People List
+        </h1>
+      </div>
+      <div className="flex gap-2 items-center px-12 py-2">
+        {/* Prev Button */}
+        <button
+          onClick={goToPrevPage}
+          disabled={currentPage === 1}
+          className={`${
+            currentPage === 1 ? "bg-red-300" : "bg-primary hover:bg-primary/50"
+          } rounded-full px-2 py-2 text-white font-semibold`}
+        >
+          <svg
+            class="h-3 w-3 fill-white md:h-3.5 md:w-3.5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+            stroke-width="1.5"
+          >
+            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+          </svg>
+        </button>
+        <p className="text-white text-center">
+          {currentPage}/{totalPages}
+        </p>
+        {/* Next Button */}
+        <button
+          onClick={goToNextPage}
+          disabled={currentPage === totalPages}
+          className={`${
+            currentPage === totalPages
+              ? "bg-red-200"
+              : "bg-primary hover:bg-primary/50"
+          } rounded-full px-2 py-2 text-white font-semibold`}
+        >
+          <svg
+            class="h-3 w-3 fill-white md:h-3.5 md:w-3.5"
+            stroke-width="1.5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+          </svg>
+        </button>
+      </div>
+      <div className="grid grid-cols-4 gap-5 px-12 py-2">
+        {data.map((e) => (
+          <div
+            key={e.id}
+            className="w-full h-full overflow-hidden rounded-md text-white shadow-lg border border-primary hover:shadow-primary/50 hover:shadow-lg py-4"
+          >
+            {e.profile_path !== null ? (
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${e.profile_path}`}
+                className="h-96 "
+              />
+            ) : (
+              <img src="images/profile2.jpg" className="h-96 " />
+            )}
+            <div className="text-xl font-bold text-center mb-4 py-2">
+              {e?.name}
+            </div>
+            <div className="text-sm text-red-500 text-center  mb-1">
+              {e?.known_for_department}
+            </div>
+            <div className="text-sm text-center mb-1">{e?.original_name}</div>
+            <div className="text-sm text-center">{e?.popularity}</div>
           </div>
         ))}
       </div>
