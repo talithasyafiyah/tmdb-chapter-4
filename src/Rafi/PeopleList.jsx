a8ad014d0c5e2b6bf2cdccfd033ce4e2604e56f2
RafiDhafinErsamazaya <160104105+RafiDhafinErsamazaya@users.noreply.github.com>
2024-03-26 20:35:06 +0700

    Update PeopleList.jsx

--- src/Rafi/PeopleList.jsx
+++ src/Rafi/PeopleList.jsx
@@ -1,5 +1,6 @@
 import react, { useEffect, useState } from "react";
 import axios from "axios";
+,
 export default function PeopleList() {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
