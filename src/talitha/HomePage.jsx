d14adf26b51cd77074baab4cecac033f763afb3b
talitha <talithasyafiyah1112@gmail.com>
2024-03-26 17:13:41 +0700

    feature: added link to search

--- src/talitha/HomePage.jsx
+++ src/talitha/HomePage.jsx
@@ -64,47 +64,61 @@ function HomePage() {
 
       {/* List Movie */}
       <section id="movie" className="container mt-60 mb-20">
-        <div className="flex justify-between">
-          <h1 className="text-lg font-bold text-white mb-6">Movie List</h1>
-          <div>
-            {/* Prev Button */}
-            <button
-              onClick={goToPrevPage}
-              disabled={currentPage === 1}
-              className={`${
-                currentPage === 1
-                  ? "bg-red-300"
-                  : "bg-primary hover:bg-primary/50"
-              } rounded-full px-2 py-2 text-white font-semibold mr-2`}
-            >
-              <svg
-                class="h-3 w-3 fill-white md:h-3.5 md:w-3.5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-                stroke-width="1.5"
+        <div className="mb-6">
+          <div className="flex justify-between items-center">
+            <h1 className="text-lg font-bold text-white">Movie List</h1>
+            <div>
+              {/* Prev Button */}
+              <button
+                onClick={goToPrevPage}
+                disabled={currentPage === 1}
+                className={`${
+                  currentPage === 1
+                    ? "bg-red-300"
+                    : "bg-primary hover:bg-primary/50"
+                } rounded-full px-2 py-2 text-white font-semibold mr-2`}
               >
-                <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-              </svg>
-            </button>
-            {/* Next Button */}
-            <button
-              onClick={goToNextPage}
-              disabled={currentPage === totalPages}
-              className={`${
-                currentPage === totalPages
-                  ? "bg-red-200"
-                  : "bg-primary hover:bg-primary/50"
-              } rounded-full px-2 py-2 text-white font-semibold`}
-            >
-              <svg
-                class="h-3 w-3 fill-white md:h-3.5 md:w-3.5"
-                stroke-width="1.5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
+                <svg
+                  class="h-3 w-3 fill-white md:h-3.5 md:w-3.5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                  stroke-width="1.5"
+                >
+                  <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+                </svg>
+              </button>
+              {/* Next Button */}
+              <button
+                onClick={goToNextPage}
+                disabled={currentPage === totalPages}
+                className={`${
+                  currentPage === totalPages
+                    ? "bg-red-200"
+                    : "bg-primary hover:bg-primary/50"
+                } rounded-full px-2 py-2 text-white font-semibold`}
               >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-              </svg>
-            </button>
+                <svg
+                  class="h-3 w-3 fill-white md:h-3.5 md:w-3.5"
+                  stroke-width="1.5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="flex gap-2 items-center">
+            <p className="text-white font-normal text-base cursor-pointer hover:text-primary hover:font-semibold">
+              Discover more movies
+            </p>
+            <svg
+              className="h-4 w-4 fill-primary cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+            </svg>
           </div>
         </div>
 
